c76b344d5304273e598007c5ee95911b08408912
johanrobayo01@gmail.com <johanrobayo01@gmail.com>
2024-06-04 15:55:45 -0500

    inicio

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CompanyJLS.Models;
+using System.Security.Cryptography;
+
+namespace CompanyJLS.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Usuario>(tb =>
+            {
+                tb.HasKey(col => col.id);
+                tb.Property(col => col.id).UseIdentityColumn()
+                .ValueGeneratedOnAdd();
+
+                tb.Property(col => col.NombreCompleto).HasMaxLength(100);
+                tb.Property(col => col.Correo).HasMaxLength(100);
+                tb.Property(col => col.Contraseña).HasMaxLength(100);
+
+            });
+
+            modelBuilder.Entity<Usuario>().ToTable("Usuario");
+        }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,12 @@
+﻿namespace CompanyJLS.Models
+{
+    public class Usuario
+    {
+        public int id {  get; set; }
+        public string NombreCompleto { get; set; }
+
+        public string Correo { get; set; }
+
+        public string Contraseña{ get; set;}
+    }
+}
